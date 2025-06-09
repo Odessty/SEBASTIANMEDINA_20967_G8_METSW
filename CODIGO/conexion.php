9c178781003f772a673d2f85c2410f6d15fb702a
Gonzalo Zavala <209189380+GonzaloZavalaA@users.noreply.github.com>
2025-06-09 11:36:08 -0500

    Delete CODIGO/conexion.php

--- CODIGO/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'control_plagas');
-if ($conn->connect_error) {
-    // Lanza una excepción o maneja el error sin imprimir nada directamente
-    error_log('Conexión fallida: ' . $conn->connect_error); // solo para el log
-    header('HTTP/1.1 500 Internal Server Error');
-    exit; // salir silenciosamente
-}
-?>
